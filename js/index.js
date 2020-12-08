0635027d67339f8c89a181e739e4e14215bf76e5
Jeriah Bowers <jeriah97bowers@yahoo.com>
2020-12-07 22:23:49 -0500

    styling reviews section

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        prevArrow: '<div class="prevButton"><img src="../images/left-arrow.svg"/></div>',
+        nextArrow: '<div class="nextButton"><img src="../images/right-arrow.svg"/></div>'
+    });
+});
